1f9626d0730fb246ea595ac9513269438e8a183c
duckyduck01 <madhvetipathak2006@gmail.com>
2026-08-11 23:58:01 +0530

    added throttle and permissions for candidate app

--- candidate/views.py
+++ candidate/views.py
@@ -1,16 +1,26 @@
 from candidate.models import Application
 from candidate.serializers import ApplicationSerializer, ApplicationWriteSerializer
 from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveDestroyAPIView
-
+from job.throttling import GeneralThrottle
+from job.permissions import IsCandidate, IsRecruiterAndCandidate
 
 class ApplicationListAPI(ListAPIView):
+    throttle_classes=[GeneralThrottle]
+    permission_classes=[IsRecruiterAndCandidate]
     serializer_class=ApplicationSerializer
     queryset=Application.objects.all()
 
 class ApplicationCreateAPI(CreateAPIView):
+    throttle_classes=[GeneralThrottle]
+    permission_classes=[IsCandidate]
     serializer_class=ApplicationWriteSerializer
     queryset=Application.objects.all()
 
 class ApplicationDetailAPI(RetrieveDestroyAPIView):
+    throttle_classes=[GeneralThrottle]
+    def get_permissions(self):
+        if self.request.method=='GET':
+            return [IsRecruiterAndCandidate()]
+        return [IsCandidate()]
     serializer_class=ApplicationSerializer
     queryset=Application.objects.all()
