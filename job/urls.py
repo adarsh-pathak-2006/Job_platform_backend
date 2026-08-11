0c5690958cda9819084902cb828dc79afb3cf04f
duckyduck01 <madhvetipathak2006@gmail.com>
2026-08-12 00:19:01 +0530

    added projects core urls..moving to migration and testing

--- job/urls.py
+++ job/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from authentication.views import RegisterAPI, CustomTokenObtainView, CustomTokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', RegisterAPI.as_view(), name='register'),
+    path('token/', CustomTokenObtainView.as_view(), name='token_obtain_view'),
+    path('token/refresh/', CustomTokenRefreshView.as_view(), name='token_refresh_view'),
+    path('api/auth/', include('authentication.urls')),
+    path('api/candidate/', include('candidate.urls')),
+    path('api/recruiter/', include('recruiter.urls')),
 ]
