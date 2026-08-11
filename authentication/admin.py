76fa7cadd32fc5a06b36e5261ab2974b349c7794
duckyduck01 <madhvetipathak2006@gmail.com>
2026-08-12 00:48:55 +0530

    added recruiter profile model on admin panel

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from authentication.models import Resume, UserProfile, User, Project, Experience
+from authentication.models import Resume, UserProfile, User, Project, Experience, RecruiterProfile
 
 admin.site.register(Resume)
 admin.site.register(Experience)
 admin.site.register(Project)
 admin.site.register(User)
 admin.site.register(UserProfile)
+admin.site.register(RecruiterProfile)
