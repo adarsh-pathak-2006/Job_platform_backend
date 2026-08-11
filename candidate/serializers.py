9969e7194f92618b2549899b840def28cd951210
duckyduck01 <madhvetipathak2006@gmail.com>
2026-08-12 00:26:50 +0530

    migration successfull fixed a minor bug

--- candidate/serializers.py
+++ candidate/serializers.py
@@ -1,12 +1,12 @@
 from rest_framework.serializers import ModelSerializer, PrimaryKeyRelatedField
 from candidate.models import Application
 from authentication.serializers import UserProfileGetSerializer
-from recruiter.serializers import RecruiterGetSerializer
+from recruiter.serializers import RecruitmentSerializer
 from recruiter.models import Recruitment
 
 class ApplicationSerializer(ModelSerializer):
     user=UserProfileGetSerializer(read_only=True)
-    job=RecruiterGetSerializer(read_only=True)
+    job=RecruitmentSerializer(read_only=True)
     class Meta:
         model=Application
         fields='__all__'
